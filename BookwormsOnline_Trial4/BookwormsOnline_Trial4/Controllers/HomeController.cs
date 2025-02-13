dff1f141ed8018b5469df5d0430d0e6afece554a
Amsyar <139153900+itssamsyar@users.noreply.github.com>
2025-02-13 11:22:23 +0800

    FEATURE: Set up the Views
    - Register View
    - Login View
    - Home View

--- BookwormsOnline_Trial4/BookwormsOnline_Trial4/Controllers/HomeController.cs
+++ BookwormsOnline_Trial4/BookwormsOnline_Trial4/Controllers/HomeController.cs
@@ -12,20 +12,77 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-
+    
+    
+    // ALL MY VIEWS
+    
+    // Loads the /Home/Register.cshtml
+    public IActionResult Register()
+    {
+        return View();
+    }
+    
+    // Loads the /Home/Login.cshtml
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    // Loads the /Home/Home.cshtml (logged in view)
+    public IActionResult Home()
+    {
+        return View();
+    }
+    
+    // Loads the /Home/Index.cshtml
     public IActionResult Index()
     {
         return View();
     }
 
+    // Loads the /Home/Privacy.cshtml
     public IActionResult Privacy()
     {
         return View();
     }
-
+    
+    // Loads the Error View
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // ALL MY ACTION METHODS
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+ 
+
+    
 }
